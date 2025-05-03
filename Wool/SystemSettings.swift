1daba056e7333c2968ef93eacce00e55458ff116
Alexey Novikov <v@2dubs.com>
2025-05-02 22:10:18 -0400

    Cleanup

--- Wool/SystemSettings.swift
+++ Wool/SystemSettings.swift
@@ -1,14 +1,7 @@
 import AppKit
 
 enum SystemSettingsPane: String {
-//  case general = "com.apple.preference.general"
-//  case accessibility = "com.apple.preference.universalaccess"
-//  case securityPrivacy = "com.apple.preference.security"
   case privacyAccessibility = "com.apple.preference.security?Privacy_Accessibility"
-//  case privacyFullDiskAccess = "com.apple.preference.security?Privacy_AllFiles"
-//  case notifications = "com.apple.preference.notifications"
-//  case displays = "com.apple.preference.displays"
-//  case keyboard = "com.apple.preference.keyboard"
   
   var url: URL {
     return URL(string: "x-apple.systempreferences:\(self.rawValue)")!
